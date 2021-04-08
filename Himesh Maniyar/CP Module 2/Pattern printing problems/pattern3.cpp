16dd1d45320cdd76caa67efec1b5d362af518594
Himesh <himeshmaniyar2017@gmail.com>
2021-04-08 18:32:31 +0530

    Added CP module 1 and 2 problems

---
+++ Himesh Maniyar/CP Module 2/Pattern printing problems/pattern3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"Enter the value of N: ";
+    cin>>N;
+    for(int i=0;i<N;++i){
+        for(int j=0;j<=i;++j){
+            cout<<"*";
+        }
+        cout<<" ";
+    }
+    return 0;
+}
