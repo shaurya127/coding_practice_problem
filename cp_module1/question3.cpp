6377a9c707f1ad541005f431e875bdbc5c233ee0
Himadri Bhavsar <77726296+himadri331@users.noreply.github.com>
2021-04-12 19:21:18 +0530

    Create question3.cpp

---
+++ cp_module1/question3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1;
+    double num2;
+    double result;
+    cout<<" Enter two numbers with space:";
+    cin>>num1>> num2;
+    result=num1*num2;
+    cout<<" mutiplication of" << num1<<" and "<<num2<<" is equal to "<<result <<endl;
+    return 0;
+}
